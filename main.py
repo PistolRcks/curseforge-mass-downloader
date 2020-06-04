c65e3a1c1cca86261f81c7cdb389f2e735b00de1
PistolRcks <17933691+PistolRcks@users.noreply.github.com>
2020-06-04 13:37:54 -0500

    New branch, used for archival purposes

--- main.py
+++ main.py
@@ -1,36 +1,77 @@
 # Scrapes the Curse Forge website to download mods from a list--from CLI
 import re
+import sys
 import os
+from selenium import webdriver
 import requests as rq
-import json
+from bs4 import BeautifulSoup as bs
 
 # TODO: Will use command line arguments (later)
-PROJECT_IDS = ["235279", "227083", "248787", "228756"] # Project IDs of the mods, as shown on the mod page
+GAME = "minecraft" # Name of the game, as is shown in the address
+CONTENT_TYPE = "mc-mods" # Name of the content, as is shown in the address
+MODS = ["chisel", "baubles", "appleskin", "iron-chests"] # Name of the mod(s), as is shown in the address
 VERSION = "1.12.2" # Name of the game version
 
-for i, id in enumerate(PROJECT_IDS):
-    ticker = f"[{i+1}/{len(PROJECT_IDS)}]" # For showing which mod we're on
+downloadIDs = []
+modFilenames = []
+versionID = ""
 
-    # Get mod name, so it looks nice
-    data = json.loads(rq.get(f"https://curse.nikky.moe/api/addon/{id}").content) # Put the request's data into a Python-readable format
-    modName = data["name"]
+# Load up Firefox using Selenium
+wd = webdriver.Firefox()
 
-    # Get mod's latest version for this game version
-    print(f"{ticker} Getting mod download link for mod {modName} (ID: {id})...")
-    data = json.loads(rq.get(f"https://curse.nikky.moe/api/addon/{id}/files").content)
-    # Get the latest mod version which supports the game version (which is last in the list)
-    latestVersion = [candidate for candidate in data if VERSION in candidate["gameVersion"]][-1]
-    print(f"{ticker} Found latest mod version for mod {modName} (ID: {id})!")
+# Scrape for download IDs and filenames using Selenium
+for i, mod in enumerate(MODS):
+    ticker = f"[{i+1}/{len(MODS)}]" # For showing which mod we're on
 
-    # Download the mod
-    if os.path.isfile("mods/" + latestVersion["fileName"]): # Don't redownload mods we may already have downloaded
-        print(f"{ticker} Mod {modName} (ID: {id}) already downloaded! Skipping.")
+    baseURL = ""
+    if versionID != "": baseURL = "https://curseforge.com/" + GAME + "/" + CONTENT_TYPE + "/" + mod + "/files/all?filter-game-version=" + versionID
+    else: baseURL = "https://curseforge.com/" + GAME + "/" + CONTENT_TYPE + "/" + mod + "/files/all"
+    print(f"{ticker} Loading page for mod name {mod}...")
+    wd.get(baseURL)
+    soup = bs(wd.page_source, 'html.parser')
+    print(f"{ticker} Found mod {soup.find('h2', class_='font-bold text-lg break-all').string}")
+
+    # Find the version selector dropdown and get the version ID (if it hasn't already been found)
+    if versionID == "":
+        versionSelector = soup.find("select")
+        versionID = re.sub(r":", r"%3A", versionSelector.find("option", text = re.compile(VERSION))["value"]) # Get the ID (and fix it up so it works)
+        print(f"Found version ID {versionID}, reloading page")
+        wd.get(baseURL + "?filter-game-version=" + versionID)
+        soup = bs(wd.page_source, 'html.parser')
+
+    # Find download ID
+    id = re.findall(r"(?:[^/](?!/))+$", soup.find("a", class_="button button--hollow mr-2 button--icon-only")["href"])[0] # Gets the id from the end of the address
+    print(f"{ticker} Found download ID {str(id)}")
+    downloadIDs.append(id)
+
+    #Find filename
+    wd.get("https://curseforge.com/" + GAME + "/" + CONTENT_TYPE + "/" + mod + "/files/" + id)
+    soup = bs(wd.page_source, 'html.parser')
+    filenameDiv = soup.find("div", class_="flex flex-row md:flex-col mr-2 justify-between md:flex-col md:w-full md:justify-between")
+    filename = filenameDiv.find_all("span", class_="text-sm")[1].string
+    print(f"{ticker} Found filename {filename}")
+    modFilenames.append(filename)
+
+# We're going to need requests after this (because downloading in Selenium isn't supported)
+print("Quitting Selenium...")
+wd.quit()
+
+# Put mods into folder called `mods`
+if not os.path.isdir("mods"):
+    os.mkdir("mods")
+
+# Download files using requests
+for i, id in enumerate(downloadIDs):
+    ticker = f"[{i+1}/{len(downloadIDs)}]" # For showing which mod we're on (again)
+    if os.path.isfile("mods/" + modFilenames[i]): # Don't redownload mods we may already have downloaded
+        print(f"{ticker} Mod {MODS[i]} already downloaded! Skipping.")
     else:
-        print(f"{ticker} Starting download of mod {modName} (ID: {id})...")
-        download = rq.get(latestVersion["downloadUrl"])
+        print(f"{ticker} Starting download of mod {MODS[i]}...")
+        link = "https://edge.forgecdn.net/files/" + re.sub(r"(.{4})", r"\1/", id) + "/" + modFilenames[i] # Add a slash after the first four numbers in the id
+        download = rq.get(link)
         assert download.status_code == 200 # Make sure we're good
-        with open("mods/" + latestVersion["fileName"], "wb") as f:
+        with open("mods/" + modFilenames[i], "wb") as f:
             f.write(download.content)
-        print(f"{ticker} Finished downloading mod {modName} (ID: {id})!")
+        print(f"{ticker} Finished downloading mod {MODS[i]}!")
 
-print(f"Finished downloading all {len(PROJECT_IDS)} mods!")
+print("Finished downloading all mods!")
